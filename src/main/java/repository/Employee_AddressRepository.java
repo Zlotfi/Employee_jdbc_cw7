8fcea3f66f4dd46176257a7e0a3e114814340828
victus <zlotfi758@gmail.com>
2023-12-14 15:25:48 +0330

    add saveEmployee method

--- src/main/java/repository/Employee_AddressRepository.java
+++ src/main/java/repository/Employee_AddressRepository.java
@@ -1,4 +1,30 @@
 package repository;
 
+import connection.JdbcConnection;
+import entity.EmployeeAddress;
+import entity.Employees;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Employee_AddressRepository {
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public Employee_AddressRepository() throws SQLException {
+    }
+
+    public int saveEmployee(Employees employees) throws SQLException {
+        String add1 = "INSERT INTO employee (employee_Number, firstName, lastName, officeCode) VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add1);
+        preparedStatement.setInt(1,employees.getEmployee_Number());
+        preparedStatement.setString(2,employees.getFirstName());
+        preparedStatement.setString(3,employees.getLastName());
+        preparedStatement.setInt(4,employees.getOfficeCode());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
+
+
 }
