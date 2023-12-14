7daf1485090aef2e54ad8dd76ae30640c0f52768
victus <zlotfi758@gmail.com>
2023-12-14 14:40:14 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/EmployeeAddress.java
+++ src/main/java/entity/EmployeeAddress.java
@@ -1,4 +1,47 @@
 package entity;
 
 public class EmployeeAddress {
+    private int addressId;
+    private String city;
+    private String street;
+    private String postalCode;
+
+    public EmployeeAddress(int addressId, String city, String street, String postalCode) {
+        this.addressId = addressId;
+        this.city = city;
+        this.street = street;
+        this.postalCode = postalCode;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
