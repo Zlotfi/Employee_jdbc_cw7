7988863d2d63d2a70854c0fff787df8f3889a74a
victus <zlotfi758@gmail.com>
2023-12-14 16:35:08 +0330

    add registerEmployee method

--- src/main/java/service/Employee_AddressService.java
+++ src/main/java/service/Employee_AddressService.java
@@ -1,4 +1,22 @@
 package service;
 
+import entity.Employees;
+import repository.Employee_AddressRepository;
+
+import java.sql.SQLException;
+
 public class Employee_AddressService {
+
+    private final Employee_AddressRepository employeeAddressRepository = new Employee_AddressRepository();
+
+    public Employee_AddressService() throws SQLException {
+    }
+
+    public void registerEmployee(Employees employees) throws SQLException {
+        int result = employeeAddressRepository.saveEmployee(employees);
+        if (result != 0)
+            System.out.println(employees.getFirstName() + " successfully added to database");
+        else
+            System.out.println("ERROR!!");
+    }
 }
